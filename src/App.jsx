ed587e8cecfc7668e819a3098cc34af9ea0f6a4d
Ace <adamabdallahayln1@gmail.com>
2025-12-02 14:39:33 +0200

    Removing white space from footer

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
   }, []);
 
   return (
-    <div className="font-sans bg-white text-gray-900 py-21.5">
+    <div className="font-sans bg-white text-gray-900 pb-4">
       <Navbar />
       <Hero />
       <About />
